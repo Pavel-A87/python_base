80b81ff038cdf235fa99d72d33560738b043e2b7
Pavel Aliakseyenka <alensky27@gmail.com>
2021-10-22 18:16:13 +0300

    Done task 00_distance in less_002

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -3,7 +3,7 @@
 
 # Есть словарь координат городов
 
-sites = {
+citys = {
     'Moscow': (550, 370),
     'London': (510, 510),
     'Paris': (480, 480),
@@ -12,11 +12,31 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
+# DO здесь заполнение словаря
+from pprint import pprint
 distances = {}
+moscow = citys['Moscow']
+london = citys['London']
+paris = citys['Paris']
 
-# TODO здесь заполнение словаря
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2  ) ** .5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2  ) ** .5
+paris_london = ((paris[0] - london[0]) ** 2 + (paris[1] - london[1]) ** 2  ) ** .5
 
-print(distances)
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+
+distances['London'] = {}
+distances['London']['Moscow'] = moscow_london
+distances['London']['Paris'] = paris_london
+
+distances['Paris'] = {}
+distances['Paris']['London'] = paris_london
+distances['Paris']['Moscow'] = moscow_paris
+
+
+pprint(distances)
 
 
 
