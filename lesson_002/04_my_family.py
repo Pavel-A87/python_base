ffbb30d9d1977aa650db697183f30316fa445504
Pavel Aliakseyenka <alensky27@gmail.com>
2021-10-23 15:40:04 +0300

    Done task 04_my_family in less_002

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -4,17 +4,24 @@
 # Создайте списки:
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
-my_family = []
+my_family = ['husband', 'wife', 'daughter', 'son']
 
 
 # список списков приблизителного роста членов вашей семьи
 my_family_height = [
     # ['имя', рост],
-    [],
+    ['Pavel', 182],
+    ['Julia', 167],
+    ['Elizaveta', 165],
+    ['Mark', 87]
 ]
 
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
+print('Рост отца - ', my_family_height[0][1], 'см')
 
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+# sum_height = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1] + my_family_height[3][1]
+sum_height = sum([item[1] for item in my_family_height])
+print('Общий рост семьи - ', sum_height, 'см')
