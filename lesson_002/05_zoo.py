acda1273130d0ec39bde3f011c2f8609e67399ba
Pavel Aliakseyenka <alensky27@gmail.com>
2021-10-23 17:46:22 +0300

    Done task 04_zoo in less_002

--- lesson_002/05_zoo.py
+++ lesson_002/05_zoo.py
@@ -5,21 +5,35 @@
 
 zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 
+
 # посадите медведя (bear) между львом и кенгуру
 #  и выведите список на консоль
-# TODO здесь ваш код
+# DO здесь ваш код
+
+zoo.insert(1, 'bear')
+print(zoo)
 
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
-# TODO здесь ваш код
+# DO здесь ваш код
+zoo.extend(birds)
+print(zoo)
+
 
 # уберите слона
 #  и выведите список на консоль
-# TODO здесь ваш код
+# DO здесь ваш код
+
+zoo.pop(3)
+#  zoo.remove('elephant') //alternate
+print(zoo)
 
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
+# DO здесь ваш код
 
-
+index_lion = zoo.index('lion')
+index_lark = zoo.index('lark')
+print('Лев сидит в клетке №', index_lion+1)
+print('Жаворонок сидит в клетке №', index_lark+1)
