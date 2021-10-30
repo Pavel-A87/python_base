35023c18e281a32afbead4cdf7177593d040915e
Pavel Aliakseyenka <alensky27@gmail.com>
2021-10-30 17:57:34 +0300

    Done task 08_garden in less_02

--- lesson_002/08_garden.py
+++ lesson_002/08_garden.py
@@ -10,18 +10,35 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # создайте множество цветов, произрастающих в саду и на лугу
 # garden_set =
 # meadow_set =
-# TODO здесь ваш код
+# DO здесь ваш код
+
+garden_set = set(garden)
+meadow_set = set(meadow)
+print(garden_set)
+print(meadow_set)
 
 # выведите на консоль все виды цветов
-# TODO здесь ваш код
+# DO здесь ваш код
+
+
+all_flowers = set(garden + meadow)
+print(all_flowers)
 
 # выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
+# DO здесь ваш код
+
+and_garden_and_meadow = garden_set.intersection(meadow_set)
+print(and_garden_and_meadow)
+
 
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
+# DO здесь ваш код
 
+only_garden = garden_set.difference(meadow_set)
+print(only_garden)
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
+# DO здесь ваш код
 
+only_meadow =meadow_set.difference(garden_set)
+print(only_meadow)
 
