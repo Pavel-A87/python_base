064e56ddeda6a213feca183264e096d09fd01d42
Pavel Aliakseyenka <alensky27@gmail.com>
2021-12-20 01:40:58 +0300

    Complit 2 tasks from less_003 ex. 00_bubbles

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -1,14 +1,35 @@
 # -*- coding: utf-8 -*-
 
+
+
+# Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
+# DO здесь ваш код
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+point = sd.get_point(100, 100)
+radius = 50
+for _ in range(3):
+    radius += 5
+    sd.circle(center_position=point, radius=radius, width=2)
+
+sd.pause()
+
+# Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
+# DO здесь ваш код
+
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
-# Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
-
-# Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+def bubble(point, step):
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(center_position=point, radius=radius, width=2)
+point = sd.get_point(300, 300)
+buble(point=point, step=10)
+sd.pause()
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
