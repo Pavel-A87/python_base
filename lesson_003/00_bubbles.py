d2c7a6eb57c4e208a14e95b7cf35ee64447be49f
Pavel Aliakseyenka <alensky27@gmail.com>
2021-12-26 11:39:52 +0300

    Complit all tasks from less_003 ex. 00_bubbles

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -9,10 +9,10 @@ import simple_draw as sd
 sd.resolution = (1200, 600)
 point = sd.get_point(100, 100)
 radius = 50
-for _ in range(3):
-    radius += 5
-    sd.circle(center_position=point, radius=radius, width=2)
 
+for _ in range(3):
+    sd.circle(center_position=point, radius=radius, width=2)
+    radius += 5
 sd.pause()
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
@@ -25,21 +25,66 @@ sd.resolution = (1200, 600)
 def bubble(point, step):
     radius = 50
     for _ in range(3):
-        radius += step
         sd.circle(center_position=point, radius=radius, width=2)
-point = sd.get_point(300, 300)
+        radius += step
+
+point = sd.get_point(100, 100)
 bubble(point=point, step=10)
 sd.pause()
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+# DO здесь ваш код
+
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+
+def bubble(point, step):
+    radius = 55
+    for _ in range(3):
+        sd.circle(center_position=point, radius=radius, width=2)
+        radius += step
+
+for x in range(100, 1001, 100):
+    point = sd.get_point(x, 100)
+    bubble(point=point, step=5)
+sd.pause()
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+# DO здесь ваш код
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+
+def bubble(point, step):
+    radius = 55
+    for _ in range(3):
+        sd.circle(center_position=point, radius=radius, width=2)
+        radius += step
+for y in range(100, 301, 100):
+    for x in range(100, 1001, 100):
+        point = sd.get_point(x, y)
+        bubble(point=point, step=5)
+sd.pause()
+
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+# DO здесь ваш код
 
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+
+def bubble(point, step):
+    radius = 50
+    for _ in range(3):
+        sd.circle(center_position=point, radius=radius, width=2)
+        radius += step
+
+for _ in range(100):
+    point = sd.random_point()
+    bubble(point=point, step=5)
 sd.pause()
+
 
 
