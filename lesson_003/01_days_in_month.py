b5efe7e3a76aa7058bb43787c5e4babb3c28245b
Pavel Aliakseyenka <alensky27@gmail.com>
2021-12-26 17:43:50 +0300

    Done tasks ex. 01_days_in_month from less_003

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -7,8 +7,21 @@
 # Если номер месяца некорректен - сообщить об этом
 
 # Номер месяца получать от пользователя следующим образом
-user_input = input("Введите, пожалуйста, номер месяца: ")
-month = int(user_input)
-print('Вы ввели', month)
+# from typing import Tuple
+# DO здесь ваш код
 
-# TODO здесь ваш код
+month_31_day = (1, 3, 5, 8, 7, 10, 12)
+month_30_day = (4, 6, 9, 11)
+month_28_day = (2,)
+while 1 > 0:
+    user_input = input("Введите, пожалуйста, номер месяца: ")
+    month = int(user_input)
+    print('Вы ввели', month)
+    if month in month_31_day:
+        print("31")
+    elif month in month_30_day:
+        print("30")
+    elif month in month_28_day:
+        print("28")
+    elif month >= 13 or month <= 0:
+        print("Неправильный месяц, должен быть положительным числом от 1 до 12")
