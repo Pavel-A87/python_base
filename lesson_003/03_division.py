13fbfe9050168958622ec6c0c9ec5b0393be5bec
Pavel Aliakseyenka <alensky27@gmail.com>
2021-12-27 22:30:38 +0300

    Init tasks ex. 03_division from less_003. Something wrong(((

--- lesson_003/03_division.py
+++ lesson_003/03_division.py
@@ -10,4 +10,15 @@
 
 a, b = 179, 37
 
-# TODO здесь ваш код
+# DO здесь ваш код
+while True:
+    a = int(input("Inter value A:"))
+    b = int(input("Inter value B:"))
+    if a < b:
+        print("Wrong values! 'A' must be a bigger then 'B'")
+    elif a > b:
+        x = a / b
+        if type(x) == int:
+            print(x)
+        else:
+            print("bbb")
